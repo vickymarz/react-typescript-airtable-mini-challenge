d39d8087c750d805587e72a7d77299d76a1ac1d1
unknown <okorojiebube2@gmail.com>
2022-05-15 08:07:20 -0600

    Configure store

--- src/features/airtable/airtableSlice.ts
+++ src/features/airtable/airtableSlice.ts
@@ -1,28 +1,25 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState, AppThunk } from '../../app/store'
+import getStudentsFromClass from './services/studentServices' 
 
+const GET_STUDENT = 'mini-extension/student/GET_STUDENT'
+const LOGOUT = 'mini-extension/student/LOGOUT'
 
 export type RecordState = {
-	record: any[]
+	records: any[]
 }
 
 const initialState: RecordState = {
-	record: [],
+	records: [],
 }
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-export const incrementAsync = createAsyncThunk('counter/fetchCount', async (amount: number) => {
-	const response = await fetchCount(amount)
-	// The value we return becomes the `fulfilled` action payload
-	return response.data
+export const incrementAsync = createAsyncThunk(GET_STUDENT, async (name: string) => {
+	const result = await getStudentsFromClass(name)
+	return result
 })
 
 export const counterSlice = createSlice({
-	name: 'counter',
+	name: 'records',
 	initialState,
 	// The `reducers` field lets us define reducers and generate associated actions
 	reducers: {
